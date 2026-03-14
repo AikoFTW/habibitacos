2aaed2ae716bf5e79a06c382f2e29f994394411a
AikoFTW <130309866+AikoFTW@users.noreply.github.com>
2026-03-13 22:47:53 -0700

    port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    PORT: 3000,
+    PORT: 3003,
     viewsPath: path.join(__dirname, 'views'),
     staticAssetsPath: path.join(__dirname, 'assets')
 };
